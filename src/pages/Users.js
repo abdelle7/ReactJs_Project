99627382d5cdd4e6e1b8687c8cf5b8ca27de7fdc
Abdellah Baybay <abdellah@exvivo.cloud>
2019-10-10 17:28:27 +0100

    Fix Local Fetch

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -2,9 +2,34 @@ import React, { Component } from "react";
 import EnhancedTable from "../components/EnhancedTable";
 import ModalPop from "../components/Modal";
 import { connect } from "react-redux";
+import { DataBase } from "../pages/const";
+import { StitchAuthInfo } from "../pages/const";
+import { stitchClient } from "../pages/const";
+import { RemoteMongoClient } from "mongodb-stitch-browser-sdk";
 
 const email = localStorage.getItem("email");
-function TestLocal() {
+const mongodb = stitchClient.getServiceClient(
+  RemoteMongoClient.factory,
+  "mongodb-atlas"
+);
+const db = mongodb.db(DataBase);
+const collection = db.collection("Utilisateur");
+
+const fetchDataLocal = () => {
+  const Session = localStorage.getItem(StitchAuthInfo);
+  if (Session !== null) {
+    collection
+      .find()
+      .toArray()
+      .then(items => {
+        localStorage.setItem("DataTable", JSON.stringify(items));
+        return items;
+      })
+      .catch(err => console.error(`Failed to find documents: ${err}`));
+  }
+};
+
+function GetDataLocal() {
   if (JSON.parse(localStorage.getItem("DataTable")) === null) {
     const rows1 = [];
     return rows1;
@@ -13,18 +38,23 @@ function TestLocal() {
     return rows1;
   }
 }
+
 class Users extends Component {
   componentDidMount() {
     this.props.dispatch({ type: "GET_ALL_DATA" });
+    //set All Users in LocalStorage
+    fetchDataLocal();
   }
+
   constructor() {
     super();
     if (email === null) {
       window.location = "/";
     }
   }
+
   render() {
-    const rows = TestLocal();
+    const rows = GetDataLocal();
     //const rows = this.props.AllUsers;
     return (
       <div className="bgcolor w-100 d-inline-flex ">
